4d5d2646bf937266b0734ef83421f7a134bdcd3c
Gabriel Lagrotteria <gabriellagrotteria18@gmail.com>
2023-10-05 12:07:09 -0300

    Se soluciono lo de los productos con code duplicado

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -57,6 +57,15 @@ router.post('/', async (req, res) => {
                 newId++;
         }
 
+        // Verificar la existencia del "code" en productos existentes
+        const existingCodes = products.map(p => p.code);
+        if (existingCodes.includes(product.code)) {
+            return res.status(409).send({
+                status: 'error',
+                error: 'El producto con este código ya existe.'
+            });
+        }
+
         // Asignar el "id" encontrado al producto
         product.id = newId;
 
